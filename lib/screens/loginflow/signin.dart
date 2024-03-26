f9d4f80cd1081d44eae9cd73493eb7ac8ed51f24
Vidhyadhar Reddy kudumula <kudumala123@gmail.com>
2024-03-26 23:23:51 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   lib/main.dart
    	modified:   lib/screens/loginflow/signin.dart

--- lib/screens/loginflow/signin.dart
+++ lib/screens/loginflow/signin.dart
@@ -1,13 +1,90 @@
-// ignore_for_file: camel_case_types
-
 import 'package:flutter/material.dart';
 import 'package:yaarit/screens/loginflow/socials.dart';
 
-class signin extends StatelessWidget {
-  const signin({super.key});
+class SigninPage extends StatelessWidget {
+  const SigninPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 150),
+            const Center(
+              child: Text(
+                'Sign In',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 38,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+            const SizedBox(height: 70),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Implement sign-in functionality here
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+              ),
+              // style: ElevatedButton.styleFrom(
+              //   textStyle: const TextStyle(
+              //     color: Colors.black, // Set text color to black
+              //   ),
+              // ),
+              child: const Text('Sign In',
+              style: TextStyle(color: Colors.black),),
+            ),
+            const SizedBox(height: 16),
+            GestureDetector(
+              onTap: () {
+                // Navigate to sign-up page or implement forgot password functionality
+              },
+              child: const Text(
+                'Forgot Password?',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            const SizedBox(height: 16),
+            GoogleBtn1(
+              onPressed: () {
+                // Implement necessary code for Google sign-in
+              },
+            ),
+            GitHubBtn1(
+              onPressed: () {
+                // Implement necessary code for Github sign-in
+              },
+            ),
+            AppleBtn1(
+              onPressed: () {
+                // Implement necessary code for apple sign-in
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
