baf85d6a040d291e7cec142a74bde83270f0e582
Vidhyadhar Reddy kudumula <kudumala123@gmail.com>
2024-03-26 18:13:46 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   lib/main.dart
    	renamed:    screens/signin.dart -> lib/screens/loginflow/signin.dart
    	new file:   lib/screens/loginflow/socials.dart

--- lib/screens/loginflow/signin.dart
+++ lib/screens/loginflow/signin.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: camel_case_types
 
 import 'package:flutter/material.dart';
+import 'package:yaarit/screens/loginflow/socials.dart';
 
 class signin extends StatelessWidget {
   const signin({super.key});
